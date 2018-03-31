13d339a4959601f686586502817b7a888f38130f
geforcexfx <geforcexfx@yahoo.com>
2018-03-31 20:51:41 +0200

    update buttons

--- src/WebApi/wwwroot/js/app/components/iot/iotapp.js
+++ src/WebApi/wwwroot/js/app/components/iot/iotapp.js
@@ -10,7 +10,8 @@ define(['knockout', 'dataservice', 'config', 'postbox', 'toastr'],
             var getJson = ko.observable(params ? params.txtiot : undefined);
             var page = ko.observable();
             var nextUrl = ko.observable();
-            var lu = ko.observable()
+            var lu = ko.observable();
+            var selected = ko.observable();
             //var vOneLS = localStorage.getItem("vOneLocalStorage ");
             
             /*while (typeof window.data3 == 'undefined') {
@@ -19,7 +20,9 @@ define(['knockout', 'dataservice', 'config', 'postbox', 'toastr'],
             //console.log(window.data3);
             
             //sdata(window.data3);
-            
+            var buttonText = ko.observable();
+
+            var changeButtonText = ko.observable();
             
             var s = document.createElement('script');
             s.type = 'text/javascript';
@@ -27,25 +30,57 @@ define(['knockout', 'dataservice', 'config', 'postbox', 'toastr'],
             var h = document.getElementsByTagName('script')[0];
             h.parentNode.insertBefore(s, h);
             
-            var changeButtonText = function () {
-                
-                };
+            
             var setData = function (result) {};
            
             var change1 = function () {
                 
             }
-            console.log(myCallbackFunction());
-            //console.log(tryiot());
-            /*
-            function myCallbackFunction(data) {
-                console.log(data);
+            changeButtonText = function () {
+                if (buttonText().localeCompare('ON') == 0) {
+                    buttonText('OFF');
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', 'http://192.168.1.56:8089/LED=ON/', true);
+                    xhr.send();
+                    
+                    var s = document.createElement('script');
+                    s.type = 'text/javascript';
+                    s.src = 'http://192.168.1.56:8089?callback=iotapp.myCallbackFunction';
+                    var h = document.getElementsByTagName('script')[0];
+                    h.parentNode.insertBefore(s, h);
+
+                    setTimeout(function () {
+                        console.log(data1);
+                        sdata(data1);
+                        console.log(buttonText());
+
+                    }, 1000);
+                    //sdata(data1);
+                    //console.log(data1);
+                }
+                else {
+                    buttonText('ON');
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', 'http://192.168.1.56:8089/LED=OFF/', true);
+                    xhr.send();
+
+                    var s = document.createElement('script');
+                    s.type = 'text/javascript';
+                    s.src = 'http://192.168.1.56:8089?callback=iotapp.myCallbackFunction';
+                    var h = document.getElementsByTagName('script')[0];
+                    h.parentNode.insertBefore(s, h);
+
+                    setTimeout(function () {
+                        console.log(data1);
+                        sdata(data1);
+                        console.log(buttonText());
+
+                    }, 1000);
+                    //sdata(data1);
+                    //console.log(data1);
+                }
             }
-            function myCallbackFunction(data) {
-                $('body').text(data);
-            }
-            */
-            //SeeMore2(window.data2);
+           
             var change = function () {
                 //myCall.clickB();
                 //console.log(window.data1);
@@ -58,13 +93,23 @@ define(['knockout', 'dataservice', 'config', 'postbox', 'toastr'],
             setTimeout(function () {
                 console.log(data1);
                 sdata(data1);
+                
+                if (data1.stat == 0) {
+                    buttonText('OFF');
+                    
+                }
+                else {
+                    buttonText('ON');
+                  
+                }
+                console.log(buttonText());
+                
             }, 2000);
             
-               // console.log(change());
-                //var buttonText = ko.observable(change());
+              
             return {
                 demo1,
-                demo2, SeeMore2, change, changeButtonText, sdata
+                demo2, SeeMore2, change, changeButtonText, sdata, buttonText
 
             };
         };
